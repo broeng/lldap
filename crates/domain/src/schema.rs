1d5c387a5ac764ee6287ac40924181baf1b13bb7
Toby <80796996+T0byV@users.noreply.github.com>
2025-01-08 12:47:11 +0100

    ldap: add support for Subschema requests

--- crates/domain/src/schema.rs
+++ crates/domain/src/schema.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 use crate::types::{AttributeName, AttributeType, LdapObjectClass};
+use itertools::Itertools;
 
 #[derive(PartialEq, Eq, Debug, Serialize, Deserialize, Clone)]
 pub struct Schema {
@@ -35,5 +36,15 @@ impl AttributeList {
     pub fn get_attribute_type(&self, name: &AttributeName) -> Option<(AttributeType, bool)> {
         self.get_attribute_schema(name)
             .map(|a| (a.attribute_type, a.is_list))
+    }
+
+    pub fn format_for_ldap_schema_description(&self) -> String {
+        // TODO: Attributes should probably be ensured to be unique at creation.
+        self.attributes
+            .iter()
+            .map(|a| a.name.as_str())
+            .unique()
+            .collect::<Vec<_>>()
+            .join(" $ ")
     }
 }
